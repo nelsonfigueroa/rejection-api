788b15d8e26547e5c07309b790546ac0899810ff
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-07-27 14:00:43 -0700

    new rejection

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,3 @@ rejections = CSV.read('db/rejections.csv')
 rejections.each do |rejection|
   Rejection.create(message: rejection[1])
 end
-
-# for some reason there's a nil entry at the end
-Rejection.last.destroy
